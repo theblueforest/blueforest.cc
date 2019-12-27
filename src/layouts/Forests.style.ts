2a1d0bb8a04f74de8aefc5f264f19ef2fc347369
Jeremy Levy <jeremy.levy@zei-world.com>
2019-12-27 19:49:24 +0100

    Forests

--- src/layouts/Forests.style.ts
+++ src/layouts/Forests.style.ts
@@ -1,12 +1,83 @@
 import { BlueForest } from "../bundle"
+import { LeavesValues } from "../values/leaves"
+import { TitlesValues } from "../values/titles"
 
 export const ForestsLayoutStyle = BlueForest.StyleSheet(({ sizes, colors }) => ({
-
-  container: [
+  container: [{
+    style: {
+      position: "relative",
+    }
+  }],
+  
+  leaves1: [
     {
       style: {
+        width: LeavesValues.width[1],
+        position: "absolute",
+        top: "220px",
+        zIndex: 0,
       },
     },
   ],
+
+  title: [
+    { style: TitlesValues.global },
+    { style: TitlesValues.onLeft },
+    {
+      style: {
+        color: colors.PINK,
+      },
+    },
+  ],
+
+  description: [{
+    style: {
+      display: "block",
+      fontFamily: "Raleway",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "24px",
+      lineLeight: "28px",
+      color: "#0244C6",
+      width: "64%",
+      margin: "0 auto 40px auto",
+    }
+  }],
+
+  image: [
+    {
+      style: {
+        width: "60%",
+        display: "block",
+        margin: "0 auto 70px",
+      },
+    },
+  ],
+
+  cardsContainer: [{
+    style: {
+      display: "flex",
+      width: "64%",
+      margin: "0 auto 70px",
+      flexWrap: "wrap",
+      justifyContent: "space-around"
+    }
+  }],
+
+  card: [{
+    style: {
+      width: "339px",
+      height: "374px",
+      left: "362px",
+      top: "369px",
+      background: "#FFFFFF",
+      boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
+      borderRadius: "5px",
+      flexShrink: 0,
+      flexGrow: 0,
+      flexBasis: "339px",
+      margin: "80px 0"
+    }
+  }]
 
 }))
--- src/layouts/Forests.tsx
+++ src/layouts/Forests.tsx
@@ -4,6 +4,7 @@ import { BlueForest } from "../bundle"
 import { i18nForests } from "../i18n/forests"
 import leaves3Image from "../../assets/leaves3.png"
 import leaves4Image from "../../assets/leaves4.png"
+import { ForestsLayoutStyle } from "./Forests.style"
 
 interface Props extends Kiwi.ComponentProps {
   keyPrefix: string
@@ -13,12 +14,44 @@ export const ForestsLayout = BlueForest.Layout<Props>({
 
   render: ({ props }) => {
     const { keyPrefix } = props
-    return <Kiwi.Container>
-      <Kiwi.Text keyPrefix={keyPrefix} i18n={i18nForests.title}/>
-      <Kiwi.Text keyPrefix={keyPrefix} i18n={i18nForests.description}/>
-      FORESTS
-      <Kiwi.Image source={leaves3Image}/>
-      <Kiwi.Image source={leaves4Image}/>
+    return <Kiwi.Container style={ForestsLayoutStyle.container}>
+      <Kiwi.Image
+        source={leaves3Image}
+        style={ForestsLayoutStyle.leaves1}
+      />
+      <Kiwi.Text
+        keyPrefix={keyPrefix}
+        i18n={i18nForests.title}
+        style={ForestsLayoutStyle.title}
+      />
+      <Kiwi.Text
+        keyPrefix={keyPrefix}
+        i18n={i18nForests.description}
+        style={ForestsLayoutStyle.description}
+      />
+
+      <Kiwi.Container style={ForestsLayoutStyle.cardsContainer}>
+        <Kiwi.Container style={ForestsLayoutStyle.card}>
+        
+        </Kiwi.Container>
+
+        <Kiwi.Container style={ForestsLayoutStyle.card}>
+        
+        </Kiwi.Container>
+
+        <Kiwi.Container style={ForestsLayoutStyle.card}>
+        
+        </Kiwi.Container>
+
+        <Kiwi.Container style={ForestsLayoutStyle.card}>
+        
+        </Kiwi.Container>
+      </Kiwi.Container>
+
+      <Kiwi.Image
+        source={leaves4Image}
+        style={ForestsLayoutStyle.image}
+      />
     </Kiwi.Container>
   }
 
