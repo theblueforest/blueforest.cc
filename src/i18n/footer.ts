32ee4c83570fd7390e9d3a7e360c626b3d91be6d
Jeremy Levy <jeremy.levy@zei-world.com>
2020-01-02 11:58:03 +0100

    Ajout de l'i18n

--- src/i18n/footer.ts
+++ src/i18n/footer.ts
@@ -6,12 +6,39 @@ export const i18nFooter = i18nData({
     fr: "Une question ?"
   },
 
-  baseline: {
+  subtitle: {
     fr: "Nous y répondrons avec plaisir",
   },
 
   legalInformations: {
-    fr: "Mentions légales",
+    fr: "[Mentions légales](https://qapex.com)",
   },
 
+  ariane: {
+    fr: "Ariane Save"
+  },
+
+  arianeRole: {
+    fr: "Designer graphique"
+  },
+
+  arianeEmail: {
+    fr: "ariane@blueforest.fr"
+  },
+
+  nazim: {
+    fr: "Nazim Lachter"
+  },
+
+  nazimRole: {
+    fr: "Dev-ops"
+  },
+
+  nazimEmail: {
+    fr: "nazim@blueforest.fr"
+  },
+
+  copyright: {
+    fr: "Copyright (c) 2019 Blue Forest"
+  }
 })
