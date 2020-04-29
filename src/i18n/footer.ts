4318aeba0dd35f6ffa92997416a414fdf941140e
Ariane Save <ariane@blueforest.cc>
2020-04-29 19:52:35 +0200

    Update footer

--- src/i18n/footer.ts
+++ src/i18n/footer.ts
@@ -22,4 +22,23 @@ export const i18nFooter = i18nData({
     fr: "Développeur & DevOps"
   },
 
+  LegalInformationsTitle: {
+    fr: "Mentions Légales",
+
+  },
+
+  LegalInformationsText:  [
+    {
+      fr: "DROP’in est un Service assuré par la Société Blue Forest, SASU au capital de 2 000 euros, immatriculée au RCS de Marseille sous le numéro 849 721 360, dont le siège social est situé au 19 rue du Musée 13001 Marseille.",
+    },
+    {
+      fr: "Le Directeur de la publication est Nazim Lachter (nazim@blueforest.cc - +33 6 28 25 71 95).",
+    },
+    {
+      fr: "L’hébergement de DROP’in est opéré par Amazon Web Services, dont le siège social est situé au 67 boulevard du Général Leclerc 92110 Clichy France, avec le numéro de téléphone +33 1 46 17 10 00.",
+    },
+    
+  ],
+
+
 })
