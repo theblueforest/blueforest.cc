058d708aa8180eda101b07105c6100df5dd15f5e
Nazim Lachter <lachtater@gmail.com>
2020-05-05 16:43:28 +0200

    Default language

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,9 @@
+import { i18nSettings, Language } from "dropin-recipes"
 import { BlueForest } from "./bundle"
 import { HomePage } from "./pages/Home"
+
+i18nSettings.setDefaultLanguage(Language.FRENCH)
+i18nSettings.setCurrentLanguage(Language.FRENCH)
 
 BlueForest.Render({
   HOME: HomePage,
