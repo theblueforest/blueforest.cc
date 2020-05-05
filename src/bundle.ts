860092565e7635305db2027d8f316c88bf945c62
Ariane Save <ariane@blueforest.cc>
2020-05-05 21:24:46 +0200

    update colors

--- src/bundle.ts
+++ src/bundle.ts
@@ -13,7 +13,7 @@ export const BlueForest = new KiwiBundleReact({
     colors: {
       MAGENTA: "#D31C69",
       PINK: "#FF6F91",
-      BLUE: "#1748C7",
+      BLUE: "#0244c6",
       PURPLE: "#6355E0",
       GREEN: "linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 33.33%), rgba(97, 255, 216, 0.5)",
       GREEN2: "#DFFEF7",
